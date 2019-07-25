4543b90256cf3a7c8477445278e3b9b61b051daa
J21 <jonster1992@gmail.com>
2019-07-25 15:10:41 -0500

    blablabla

--- JavaFXtoSwing/BasicJavaSwingButtonImage/src/swing/Main.java
+++ JavaFXtoSwing/BasicJavaSwingButtonImage/src/swing/Main.java
@@ -1,20 +1,32 @@
 package swing;
 
+import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
 public class Main {
-	
+	static JLabel response;
  	public static void addComponentsToPane(Container pane) {
-		pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
-		JLabel topLabel = new JLabel("Push A Button");
-		JLabel response = new JLabel("");
-	    JButton fungi = new JButton();
-	    fungi.setText("Fungi");
-	    JButton dairy = new JButton();
+		pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));  
+		response = new JLabel("");
+		ImageIcon fImage = new ImageIcon("fungi.png");
+		Image f = fImage.getImage();
+		Image f2 = f.getScaledInstance(50, 50, 0);
+		ImageIcon f2Image = new ImageIcon(f2);
+	    JButton fungi = new JButton("Fungi", f2Image);
+	    fungi.setPreferredSize(new Dimension(100, 100));
+	    ImageIcon dImage = new ImageIcon("dairy.png");
+	    Image d = dImage.getImage();
+	    Image d2 = d.getScaledInstance(50, 50, 0);
+	   	ImageIcon d2Image = new ImageIcon(d2);
+	    JButton dairy = new JButton("Dairy", d2Image);
+	    dairy.setPreferredSize(new Dimension(100, 100));
 	    dairy.setText("Dairy");
 	    JButton reset = new JButton();
 	    reset.setText("Reset");
@@ -35,20 +47,29 @@ public class Main {
 	         	response.setText("");
 	         }
 	     });
-	     pane.add(topLabel);
 	     pane.add(fungi);
 	     pane.add(dairy);
 	     pane.add(response);
-	     pane.add(reset);
 	}
 	
  	public static void createAndShowGUI() {
  		JFrame frame = new JFrame("ButtonImage");
+ 		frame.setLayout(new BorderLayout());
+ 		JLabel toplabel = new JLabel("Push A Button");
+ 		JButton resetButton = new JButton();
+ 		resetButton.setText("Reset");
+ 		resetButton.addActionListener(new ActionListener() {
+	         public void actionPerformed(ActionEvent e) {
+		         	response.setText("");
+		         }
+		     });
  		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
+ 		frame.add(toplabel, BorderLayout.PAGE_START);
 	 	addComponentsToPane(frame.getContentPane());
 	   //Display the window.
+	 	frame.add(resetButton, BorderLayout.PAGE_END);
 	 	frame.pack();
+	 	frame.setLocationRelativeTo(null);
 	 	frame.setVisible(true);	
  	}
 	
