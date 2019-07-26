d458d637c92f85119154f99fecdaa8edfa81f13d
J21 <jonster1992@gmail.com>
2019-07-26 08:36:19 -0500

    Revising some of the files

--- JavaFXtoSwing/BasicJavaSwingDemo/src/swing/Main.java
+++ JavaFXtoSwing/BasicJavaSwingDemo/src/swing/Main.java
@@ -1,40 +1,17 @@
 package swing;
 
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /*
  * HelloWorldSwing.java requires no other files. 
  */
-import javax.swing.*;        
+import javax.swing.*;      
 
 public class Main {
 	
-	public static void centerAndSizeJFrameOnScreen(JFrame frm) {
-	    /*
-			Dimension class is a part of Java AWT. 
-	    	It contains the height and width of a 
-	    	component in integer as well as double precision. 
-	    	The use of Dimension class is that many functions of 
-			Java AWT and Swing return dimension object.
-	    */
-		// get the size of the screen
-		// Toolkit class is the abstract superclass of every implementation 
-		// in the Abstract Window Toolkit (AWT). 
-		// Subclasses of Toolkit are used to bind various components.
-		// getDefaultToolkit() returns an instance
-		// getScreenSize() is one of the many methods available
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		int height = screenSize.height;
-		int width = screenSize.width;
-		// set the size of the frame to half in width and height
-		frm.setSize(width/2, height/2);
-		// here's the part where the JFrame gets actually centered
-		frm.setLocationRelativeTo(null);
-	}
     /**
      * Create the GUI and show it.  For thread safety,
      * this method should be invoked from the
@@ -42,11 +19,10 @@ public class Main {
      */
     public static void createAndShowGUI() {
         //Create and set up the window.
-        JFrame frame = new JFrame("HelloWorldSwing");
-        centerAndSizeJFrameOnScreen(frame);
-        JPanel panel = new JPanel();
+        JFrame frame = new JFrame("HelloWorldSwingDemo1");
+        frame.setSize(300, 250);
+        frame.setLayout(new GridBagLayout());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         //Add the ubiquitous "Hello World" label.
         JButton button = new JButton("Hello World");
         button.addActionListener(new ActionListener() {
@@ -55,9 +31,7 @@ public class Main {
 				System.out.println("Hello world!");
 			}
         });
-        panel.add(button);
-        frame.getContentPane().add(panel);
-
+        frame.add(button, new GridBagConstraints());
         //Display the window.
         frame.pack();
         frame.setVisible(true);
--- JavaFXtoSwing/BasicSwingComboBox/src/swing/Main.java
+++ JavaFXtoSwing/BasicSwingComboBox/src/swing/Main.java
@@ -3,6 +3,7 @@ package swing;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
@@ -10,38 +11,50 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 class Main implements ListSelectionListener {
+		//variables
 		JList<String> jlst;
+		JFrame jfrm;
 		JLabel jlab;
+		JLabel label;
 		JComboBox box;
+		JPanel panel1, panel2, panel3, all;
 		// Create an array of names.
 		String transport[] = { "train", "car", "airplane"};
 		Main() {
 			// Create a new JFrame container.
-			JFrame jfrm = new JFrame("JList Demo");
-			// Specify a flow Layout.
-			jfrm.setLayout(new FlowLayout());
+			jfrm = new JFrame("SwingComboBox");
+			jfrm.setLocationRelativeTo(null);
+			// Specify a gridbag layout
+			jfrm.setLayout(new GridBagLayout());
 			// Give the frame an initial size.
-			jfrm.setSize(200, 160);
+			jfrm.setSize(300, 200);
 			// Terminate the program when the user closes the application.
 			jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			// Create a JList.
-			jlst = new JList<String>(transport);
-			// Set the list selection mode to single-selection.
-			jlst.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-			// Add list to a scroll pane.
+			jlab = new JLabel("Select Transport");
+			label = new JLabel(" ");
+			panel1 = new JPanel();
+			panel2 = new JPanel();
+			panel3 = new JPanel();
+			all = new JPanel();
+			all.setLayout(new BoxLayout (all, BoxLayout.Y_AXIS));
 			box = new JComboBox(transport);
-			// Set the preferred size of the scroll pane.
-			box.setPreferredSize(new Dimension(120, 30));
-			// Make a label that displays the selection.
-			jlab = new JLabel("Select transport");
-			// Add list selection handler.
-			jlst.addListSelectionListener(this);
-			// Add the list and label to the content pane.
-			jfrm.add(jlab);
-			jfrm.add(box);
-			// Display the frame.
-			jfrm.setVisible(true);
-		}
+			//set size of box
+			box.setPreferredSize(new Dimension(200, 20));
+			//cast text to item selected
+			box.addActionListener(e -> {
+				String str = (String) box.getSelectedItem();
+				label.setText("Selected Panel: " + str);
+			});
+			
+			panel1.add(jlab);
+			panel2.add(box);
+			panel3.add(label);
+			all.add(panel1);
+			all.add(panel2);
+			all.add(panel3);
+			
+			jfrm.add(all);
+			jfrm.setVisible(true);		}
 		
 		// Handle list selection events.
 		public void valueChanged(ListSelectionEvent le) {
